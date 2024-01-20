db90b46a7f2ba81f88cca34810150c834e90d482
Adi <marzukrahmanadi@gmail.com>
2024-01-21 00:21:52 +0600

    router added

---
+++ app/about/about.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+
+import { Link } from 'expo-router';
+
+
+export default function Page() {
+  return (
+    <View>
+      <Link href="/">Home</Link>
+      <Text>Hell</Text>
+    </View>
+  );
+}
